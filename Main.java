1406f16dde12046f3df63d48ded624c977dac5e2
Victor <victor.desfourneaux@gmail.com>
2018-04-02 16:49:36 +0200

    add more example points

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@
 public class Main {
 
     public static void main (String[] args) {
-        Reseaux reseau = new Reseaux();
+        Reseaux reseau = new Reseaux(4,4);
         reseau.setCouleur(0, 0, reseau.NOIR);
+        reseau.setCouleur(2, 3, reseau.NOIR);
         reseau.setCouleur(2, 2, reseau.NOIR);
+        reseau.setCouleur(3, 2, reseau.NOIR);
         reseau.setCouleur(3, 3, reseau.NOIR);
         reseau.dessinerReseau();
     }
