ee8d3f392b698c279e9fb4fa62250e7d221e3f07
Victor <victor.desfourneaux@gmail.com>
2018-04-15 12:08:07 +0200

    Ajout des méthodes d'effaçage de fenêtre Nouvelle classe UnorderedPairOfPoints

--- Fenetre.java
+++ Fenetre.java
@@ -1,6 +1,10 @@
 
+import java.util.Map;
+import java.util.Hashtable;
 import info.graphics.Color;
 import info.graphics.Point;
+import info.graphics.Line;
+import info.graphics.Ellipse;
 
 /**
  * Regroupe le réglage de la fenêtre et les méthodes de dessin.
@@ -12,6 +16,8 @@ public class Fenetre {
     public final static int MARGE = 60;
 
     private info.graphics.Window fenetre;
+    private Map<UnorderedPairOfPoints, Line> lignes;
+    private Map<Point, Ellipse> points;
     
     /**
      * Constructeur de Fenetre
@@ -24,6 +30,8 @@ public class Fenetre {
         final int largeur = (nbLignes-1)*DIMENSION_CASE + 2*MARGE;
         final int hauteur = (nbColonnes-1)*DIMENSION_CASE + 2*MARGE;
         this.fenetre = new info.graphics.Window(largeur, hauteur, titre);
+        this.lignes = new Hashtable<>();
+        this.points = new Hashtable<>();
     }
     
     /**
@@ -60,9 +68,29 @@ public class Fenetre {
         final int diametre = DIMENSION_CASE/2;
         final int rayon = diametre/2;
         Point c = Fenetre.gridToWindow(p);
-        // x et y sont les coordonnées du centre du cercle
-        // drawEllipse fonctionne en encadrant l'éllipse dans un rectangle
-        this.fenetre.drawEllipse(c.getX()-rayon, c.getY()-rayon, estPlein, Color.black, diametre, diametre);
+        Ellipse cercle = this.points.get(p);
+        if (cercle != null && cercle.isFilled() != estPlein) {
+            this.effacerPoint(p);
+            cercle = null;
+        }
+        if (cercle == null) {
+            // x et y sont les coordonnées du centre du cercle
+            // drawEllipse fonctionne en encadrant l'éllipse dans un rectangle
+            cercle = this.fenetre.drawEllipse(c.getX()-rayon, c.getY()-rayon, estPlein, Color.black, diametre, diametre);
+            this.points.put(p, cercle);
+        }
+    }
+
+    /**
+     * Efface un point
+     *
+     * @param p un point
+     */
+    public void effacerPoint(Point p) {
+        Ellipse cercle = this.points.remove(p);
+        if (cercle != null) {
+            this.fenetre.erase(cercle);
+        }
     }
 
     /**
@@ -73,12 +101,34 @@ public class Fenetre {
      * @param p2 un point
      */
     public void dessinerLigne(Point p1, Point p2) {
-        Point c1 = Fenetre.gridToWindow(p1);
-        Point c2 = Fenetre.gridToWindow(p2);
-        this.fenetre.drawLine(
-            Color.black,
-            c1.getX(), c1.getY(),
-            c2.getX(), c2.getY()
-        );
+        UnorderedPairOfPoints pair = new UnorderedPairOfPoints(p1, p2);
+        Line ligne = this.lignes.get(pair);
+        if (ligne == null) {
+            Point c1 = Fenetre.gridToWindow(p1);
+            Point c2 = Fenetre.gridToWindow(p2);
+            ligne = this.fenetre.drawLine(
+                Color.black,
+                c1.getX(), c1.getY(),
+                c2.getX(), c2.getY()
+            );
+            this.lignes.put(pair, ligne);
+        }
+    }
+
+    public void effacerLigne(Point p1, Point p2) {
+        UnorderedPairOfPoints pair = new UnorderedPairOfPoints(p1, p2);
+        Line ligne = this.lignes.remove(pair);
+        if (ligne != null) {
+            this.fenetre.erase(ligne);
+        }
+    }
+
+    public void effacerFenetre() {
+        for (Ellipse cercle : this.points.values())
+            this.fenetre.erase(cercle);
+        for (Line ligne : this.lignes.values())
+            this.fenetre.erase(ligne);
+        this.points.clear();
+        this.lignes.clear();
     }
 }
